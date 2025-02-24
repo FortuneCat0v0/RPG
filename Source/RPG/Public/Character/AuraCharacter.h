066b4bfaf0a0f1fdabbd478a8d84b5123aefc5b0
FortuneCat0v0 <1142094319@qq.com>
2025-02-24 13:53:31 +0800

    Player and Enemy Characters

---
+++ Source/RPG/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
