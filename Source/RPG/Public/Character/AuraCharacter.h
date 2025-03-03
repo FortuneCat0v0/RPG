f961fe560da23b5b6eec8194ec59cbbcd17ee9f4
FortuneCat0v0 <1142094319@qq.com>
2025-03-03 11:33:26 +0800

    10. Game Mode

--- Source/RPG/Public/Character/AuraCharacter.h
+++ Source/RPG/Public/Character/AuraCharacter.h
@@ -1,17 +1,15 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class RPG_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAuraCharacter();
 };
