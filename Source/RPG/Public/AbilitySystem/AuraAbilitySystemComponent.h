864bfc49121884df7e275c91d73c725eda90650d
FortuneCat0v0 <1142094319@qq.com>
2025-03-03 15:21:31 +0800

    4. Ability System Component and Attribute Set

---
+++ Source/RPG/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
