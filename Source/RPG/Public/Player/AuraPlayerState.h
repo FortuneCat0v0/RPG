e06a1f7ed59ff796579fff26ac701f70ad6d233c
FortuneCat0v0 <1142094319@qq.com>
2025-03-03 16:17:02 +0800

    6. Constructing the ASC and AS

--- Source/RPG/Public/Player/AuraPlayerState.h
+++ Source/RPG/Public/Player/AuraPlayerState.h
@@ -1,17 +1,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
-/**
- * 
- */
+class UAttributeSet;
+
 UCLASS()
-class RPG_API AAuraPlayerState : public APlayerState
+class RPG_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AAuraPlayerState();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
